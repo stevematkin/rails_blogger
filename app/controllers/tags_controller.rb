86a0eb09d0db0809cbf244937debba8f3e42b0cb
Steve <steve.matkin08@gmail.com>
2020-02-18 12:35:32 -0700

    Added tagging feature

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+    def index
+        @tag = Tag.all
+    end
+    
+    def show
+        @tag = Tag.find(params[:id])
+    end
+
+    def destroy
+        @tag = Tag.destroy(params[:id])
+        flash.notice = "Tag '#{@tag.name} deleted."
+        redirect_to tags_path()
+    end
+end
